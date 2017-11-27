5565f1a952dec7dbbe262e2060ec92bf688e96e7
Darkkrye <pierre.boudon.27@gmail.com>
2017-11-27 16:22:04 +0100

    Added new projet

---
+++ Mock/MockFramework/MockFramework/MockFramework.h
@@ -0,0 +1,19 @@
+//
+//  MockFramework.h
+//  MockFramework
+//
+//  Created by Openfield Mobility on 27/11/2017.
+//  Copyright © 2017 Openfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockFramework.
+FOUNDATION_EXPORT double MockFrameworkVersionNumber;
+
+//! Project version string for MockFramework.
+FOUNDATION_EXPORT const unsigned char MockFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockFramework/PublicHeader.h>
+
+
